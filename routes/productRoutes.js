00a83bf51266328db371344f0d47662e06e8a348
Ahmed9190 <ahmed.waleed919@outlook.com>
2025-02-01 15:03:54 +0200

    Remove unnecessary console log from product routes

--- routes/productRoutes.js
+++ routes/productRoutes.js
@@ -1,14 +1,12 @@
 import { Router } from "express";
 import { generatePageResponse, generateResponse } from "../utils.js";
 import { DEFAULT_PAGE_SIZE } from "../constants.js";
-import { products } from "../data/products.js"
+import { products } from "../data/products.js";
 const router = Router();
-
 
 // Endpoint for retrieving paginated products
 router.get("/", (req, res) => {
   const { Page = 1, PerPage = DEFAULT_PAGE_SIZE } = req.query;
-  console.log(req.query)
 
   const start = (Page - 1) * PerPage;
   const end = start + parseInt(PerPage);
